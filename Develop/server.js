de730214d373d7077da60f16ff4660bf3a9981c6
Tshakalisa Khupe <tkhupe@gmail.com>
2023-04-21 08:26:55 -0500

    added app.get routes

--- Develop/server.js
+++ Develop/server.js
@@ -8,3 +8,17 @@ const PORT = process.env.PORT || 5001;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listeners(PORT, () => console.log('listening on port ' + PORT));
