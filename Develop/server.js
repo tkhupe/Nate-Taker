4839dd4f3b25e31d8c63a8eaf3270a045098ae90
Tshakalisa Khupe <tkhupe@gmail.com>
2023-04-21 09:11:06 -0500

    added post route

--- Develop/server.js
+++ Develop/server.js
@@ -5,20 +5,41 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+function makeId(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('notes', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'));
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    let noteList = JSON.parse(fs.readFileSync('./db/db.json','utf8'));
+    let noteId = makeId(8);
+    newNote.id = noteId;
+    
+
+
 });
 
 app.listen(PORT, () => console.log('listening on port ' + PORT));
